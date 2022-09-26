fe3bed6db6dc590e199033df6a2891197b60ecc4
AmayuTI21A <114399243+AmayuTI21A@users.noreply.github.com>
2022-09-26 19:30:49 +0700

    Add files via upload

---
+++ TugasSesi1NO1.java
@@ -0,0 +1,12 @@
+/* 
+NAMA  : Angga Maulana Yusup
+Kelas : TI21A
+NIM   : 20210040002
+*/
+public class TugasSesi1NO1 {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
